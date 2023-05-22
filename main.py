f4817f158b884b4ae003ef6673c2e8a4e4cba6c3
Diana Cardona <dscardona@gmail.com>
2023-05-22 15:00:19 -0400

    Fix report bug by instatiating CoffeeMaker()

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+coffee_machine = CoffeeMaker()
 machine_is_on = True
 
 while machine_is_on:
