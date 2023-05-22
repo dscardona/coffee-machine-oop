364c2c45591c39bc3951c6665b72158c8f0851d9
Diana Cardona <dscardona@gmail.com>
2023-05-22 14:58:29 -0400

    Set flag for while loop

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+machine_is_on = True
 
 while machine_is_on:
     choice = input("What would you like? (espresso/latte/cappuccino/):")
