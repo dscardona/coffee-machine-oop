62f8df55ebe3ef6d1521073145deb1eb9be41bc2
Diana Cardona <dscardona@gmail.com>
2023-05-22 15:02:01 -0400

    Instantiate Menu() class, and set options dynamically

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ from money_machine import MoneyMachine
 
 coffee_machine = CoffeeMaker()
 machine_is_on = True
+menu = Menu()
 
 while machine_is_on:
-    choice = input("What would you like? (espresso/latte/cappuccino/):")
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}):")
 
     if choice == "off":
         machine_is_on = False
     elif choice == "report":
         coffee_machine.report()
+
