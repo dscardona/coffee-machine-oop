47f876d4664e700d930e0989cd63b8676b0f9141
Diana Cardona <dscardona@gmail.com>
2023-05-22 15:07:19 -0400

    Use money_machin method to validate payment, and make coffee

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 coffee_machine = CoffeeMaker()
+money_machine = MoneyMachine()
 machine_is_on = True
 menu = Menu()
 
@@ -14,6 +15,8 @@ while machine_is_on:
         machine_is_on = False
     elif choice == "report":
         coffee_machine.report()
+        money_machine.report()
     else:
         drink = menu.find_drink(choice)
-        coffee_machine.is_resource_sufficient(drink)
+        if coffee_machine.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_machine.make_coffee(drink)
